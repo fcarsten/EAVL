d629c4f636f203bd17949a396922209a0ffbb9b5
joshvote <Josh.Vote@csiro.au>
2014-12-10 15:03:38 +0800

    Made drag/drop operations optional

--- src/main/webapp/js/eavl/widgets/plugins/ModelDragDrop.js
+++ src/main/webapp/js/eavl/widgets/plugins/ModelDragDrop.js
@@ -10,8 +10,8 @@
  *   ddGroup : String - The name of the drag/drop group this will belong to
  *   highlightBody : Boolean - Should drop notifications highlight the body (better performance/interactivity). If false, drop notification will be done via a temporary mask. Default true
  *
- *   handleDrop: function(Ext.Component me, Ext.data.Model model, Ext.Component source) - A function that should handle the insertion of the specified Ext.data.Model
- *   handleDrag: function(Ext.Component me, Ext.data.Model model, Ext.Component source) - A function that should handle the removal of the specified Ext.data.Model (it's been dragged elsewhere)
+ *   handleDrop: function(Ext.Component me, Ext.data.Model model, Ext.Component source) - [optional] - A function that should handle the insertion of the specified Ext.data.Model. If omitted, this component will not receive drops
+ *   handleDrag: function(Ext.Component me, Ext.data.Model model, Ext.Component source) - [optional] - A function that should handle the removal of the specified Ext.data.Model (it's been dragged elsewhere). If omitted, this component will not support dragging
  * }
  */
 Ext.define('eavl.widgets.plugins.ModelDragDrop', {
@@ -48,77 +48,81 @@ Ext.define('eavl.widgets.plugins.ModelDragDrop', {
         var dropEl = component.getEl();
         var me = this;
 
-        this.dropTarget = new Ext.dd.DropTarget(dropEl, {
-            ddGroup : this.ddGroup,
-            notifyEnter: function(ddSource, e, data) {
-                if (data.source === me) {
-                    return;
-                }
+        if (me.handleDrop) {
+            this.dropTarget = new Ext.dd.DropTarget(dropEl, {
+                ddGroup : this.ddGroup,
+                notifyEnter: function(ddSource, e, data) {
+                    if (data.source === me) {
+                        return;
+                    }
 
-                if (me.highlightBody) {
-                    dropEl.stopAnimation();
-                    dropEl.highlight();
-                } else {
-                    eavl.widgets.util.HighlightUtil.highlight(component);
+                    if (me.highlightBody) {
+                        dropEl.stopAnimation();
+                        dropEl.highlight();
+                    } else {
+                        eavl.widgets.util.HighlightUtil.highlight(component);
+                    }
+                },
+                notifyDrop: function(ddSource, e, data) {
+                    if (data.source === me) {
+                        return;
+                    }
+                    data.source.handleDrag(data.source.component, data.draggedRecord, component);
+                    me.handleDrop(component, data.draggedRecord, data.source.component);
                 }
-            },
-            notifyDrop: function(ddSource, e, data) {
-                if (data.source === me) {
-                    return;
-                }
-                data.source.handleDrag(data.source.component, data.draggedRecord, component);
-                me.handleDrop(component, data.draggedRecord, data.source.component);
-            }
-        });
-
-        //Dragging means we have to do a bit of investigating to work out how
-        //we can generate the drag data element (ideally we want to clone it directly
-        //from the component.
-        var generateSourceEl = null;
-        var generateRecord = null;
-        if (component.getView) {
-            //Anything with a view is easy enough
-            generateSourceEl = function(e) {
-                return e.getTarget(component.getView().itemSelector, 10);
-            };
-            generateRecord = function(e, sourceEl) {
-                return component.getView().getRecord(sourceEl);
-            };
-        } else if (component.getValue) {
-            //Form fields are workable too...
-            generateSourceEl = function(e) {
-                return component.body;
-            };
-            generateRecord = function(e, sourceEl) {
-                return component.getValue();
-            }
+            });
         }
 
-        this.dragZone = new Ext.dd.DragZone(dragEl, {
-            ddGroup: this.ddGroup,
-            getDragData: function(e) {
-                var sourceEl = generateSourceEl(e);
-                if (sourceEl) {
-                    d = sourceEl.cloneNode(true);
-                    d.id = Ext.id();
-
-                    return {
-                        ddel: d,
-                        sourceEl: sourceEl,
-                        repairXY: Ext.fly(sourceEl).getXY(),
-                        source: me,
-                        draggedRecord: generateRecord(e, sourceEl)
-                    }
+        if (this.handleDrag) {
+            //Dragging means we have to do a bit of investigating to work out how
+            //we can generate the drag data element (ideally we want to clone it directly
+            //from the component.
+            var generateSourceEl = null;
+            var generateRecord = null;
+            if (component.getView) {
+                //Anything with a view is easy enough
+                generateSourceEl = function(e) {
+                    return e.getTarget(component.getView().itemSelector, 10);
+                };
+                generateRecord = function(e, sourceEl) {
+                    return component.getView().getRecord(sourceEl);
+                };
+            } else if (component.getValue) {
+                //Form fields are workable too...
+                generateSourceEl = function(e) {
+                    return component.body;
+                };
+                generateRecord = function(e, sourceEl) {
+                    return component.getValue();
                 }
-            },
-
-//          Provide coordinates for the proxy to slide back to on failed drag.
-//          This is the original XY coordinates of the draggable element captured
-//          in the getDragData method.
-            getRepairXY: function() {
-                return this.dragData.repairXY;
             }
-        });
+
+            this.dragZone = new Ext.dd.DragZone(dragEl, {
+                ddGroup: this.ddGroup,
+                getDragData: function(e) {
+                    var sourceEl = generateSourceEl(e);
+                    if (sourceEl) {
+                        d = sourceEl.cloneNode(true);
+                        d.id = Ext.id();
+
+                        return {
+                            ddel: d,
+                            sourceEl: sourceEl,
+                            repairXY: Ext.fly(sourceEl).getXY(),
+                            source: me,
+                            draggedRecord: generateRecord(e, sourceEl)
+                        }
+                    }
+                },
+
+    //          Provide coordinates for the proxy to slide back to on failed drag.
+    //          This is the original XY coordinates of the draggable element captured
+    //          in the getDragData method.
+                getRepairXY: function() {
+                    return this.dragData.repairXY;
+                }
+            });
+        }
     },
 
     beforeDestroy: function() {
