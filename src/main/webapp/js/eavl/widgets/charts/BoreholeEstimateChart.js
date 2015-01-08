f5948683bd2d02961e6381247066b4c8192aa8bf
joshvote <Josh.Vote@csiro.au>
2015-01-08 13:56:59 +0800

    EAVl-36 Fixed bad performance due to d3.append abuse.

--- src/main/webapp/js/eavl/widgets/charts/BoreholeEstimateChart.js
+++ src/main/webapp/js/eavl/widgets/charts/BoreholeEstimateChart.js
@@ -117,23 +117,34 @@ Ext.define('eavl.widgets.charts.BoreholeEstimateChart', {
                 .attr('class', 'bhe-group')
                 .attr("transform", "translate(0, " + i * this.rowHeight + ")");
 
+            //We will be rendering a LOT of rectangles. Using the d3.append for each element is
+            //a HUGE performance hit. There is no appendAll or bulkAppend so we're forced to step 
+            //outside D3js and do the appending manually using document fragments.
             var rectWidth = barWidth / bh.values.length;
+            var frag = document.createDocumentFragment(); //This is a virtual element so we can append everything in one hit
             for (var j = 0; j < bh.values.length; j++) {
-                bhGroup.append("rect")
-                     .attr('x', this.textWidth + this.rowMargin + j * rectWidth)
-                     .attr('y', this.rowMargin)
-                     .attr('width', rectWidth)
-                     .attr('estimate', bh.values[j])
-                     .attr('height', this.rowHeight - this.rowMargin * 2)
-                     .on('mouseover', tip.show)
-                     .on('mouseout', tip.hide)
-                     .attr('fill', estimateToColor(bh.values[j]));
-
+                var rectEl = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+                rectEl.setAttributeNS(null, 'x', this.textWidth + this.rowMargin + j * rectWidth);
+                rectEl.setAttributeNS(null, 'y', this.rowMargin);
+                rectEl.setAttributeNS(null, 'width', rectWidth)
+                rectEl.setAttributeNS(null, 'estimate', bh.values[j]);
+                rectEl.setAttributeNS(null, 'height', this.rowHeight - this.rowMargin * 2);
+                rectEl.setAttributeNS(null, 'fill', estimateToColor(bh.values[j]));                
+                frag.appendChild(rectEl);
             }
-
-            bhGroup.append("text")
-                .attr("transform", "translate(10," + (this.rowHeight / 2 + 8) + ")")
-                .text(bh.group);
+            
+            var textEl = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+            textEl.setAttributeNS(null, 'transform', "translate(10," + (this.rowHeight / 2 + 8) + ")");
+            textEl.appendChild(document.createTextNode(bh.group));
+            frag.appendChild(textEl);
+            
+            bhGroup[0][0].appendChild(frag);
+            
+            //The downside to using document fragments is that we dont inherit the D3 JS event wrappers
+            //which force us to "step back into" D3 for event handling
+            bhGroup.selectAll("rect")
+                .on('mouseover', tip.show)
+                .on('mouseout', tip.hide);
         }
     }
 });
